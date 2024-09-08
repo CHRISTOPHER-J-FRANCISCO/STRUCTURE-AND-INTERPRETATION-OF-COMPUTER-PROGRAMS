db1748a83e695a70cd563350ad45d4f619487004
Christopher <cjfg00gl3@gmail.com>
2024-09-07 20:14:19 -0400

    UPDATE circleAreaApproximationAgain.lisp including a substitution model example

--- THE-ELEMENTS-OF-PROGRAMMING/circleAreaApproximationAgain.lisp
+++ THE-ELEMENTS-OF-PROGRAMMING/circleAreaApproximationAgain.lisp
@@ -1,4 +1,16 @@
+; compound procedure with rubbish calculation of the area of a circle
 (defun calculateCircleAreaAppoximation (radius)
     (* radius (* radius (/ 22.0 7))))
 
 (princ (calculateCircleAreaAppoximation 4))
+
+; use the substitution model
+
+(princ " ")
+(princ  (* 4 (* 4 (/ 22.0 7))))
+(princ " ")
+(princ  (* 4 (* 4 3.142857)))
+(princ " ")
+(princ  (* 4 12.571428))
+(princ " ")
+(princ 50.285713)
