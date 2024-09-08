e4517ef272e8fadf8b829c5e77078640c9ad96e0
Christopher <cjfg00gl3@gmail.com>
2024-09-07 22:18:31 -0400

    UPDATE squareRootApproximation.lisp

--- THE-ELEMENTS-OF-PROGRAMMING/squareRootApproximation.lisp
+++ THE-ELEMENTS-OF-PROGRAMMING/squareRootApproximation.lisp
@@ -2,7 +2,7 @@
 (defun squareRoot (x )
   (let ((guess 1.0)) ; cant use defvar because it would be hidden from the loop
     (loop for i from 1 to 100
-        do (setf guess (/ (+ (/ x guess) guess) 2)))
+        do (setf guess (/ (+ (/ x guess) guess) 2))) ; divide add average
     guess))
 
 (princ (squareRoot 2))
